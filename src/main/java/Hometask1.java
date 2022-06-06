6baed77cf05f9b5dfb96d4a2d7890c8c931bf7fe
olgaber <olgaber@users.noreply.github.com>
2022-06-06 23:17:49 +0200

    Update Hometask1.java
    @mkarpenkoProvectus Please review home task for session 2

--- src/main/java/Hometask1.java
+++ src/main/java/Hometask1.java
@@ -42,4 +42,5 @@ public class Hometask1 {
         System.out.println(c + " degrees Celsius equal to " + k + " kelvins" );
         System.out.println(c + " degrees Celsius equal to " + f + " degrees Fahrenheit");
     }
+    
 }
